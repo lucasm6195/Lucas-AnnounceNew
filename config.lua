ee2f59fd825cdd620c5f44792cbc6145bab48391
Owni <120651078+mano6195@users.noreply.github.com>
2024-05-11 22:11:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ Config.JobNames = {
 }
 
 Config.JobImages = {
-    police = "https://media.discordapp.net/attachments/1203778681940742194/1238943355577045114/latest.png?ex=66411f6a&is=663fcdea&hm=4d2f32661ce12f1c6b2af9f0b278ce8bd6c6daad5e25e1e9c85a2c12f684b020&=&format=webp&quality=lossless",
+    police = "",
     ambulance = "",
     mechanic = "",
     taxi = "",
